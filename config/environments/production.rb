213fda9791057241f5b33e0e4ec4bdd02852f7e6
Charles <charlescampet03@gmail.com>
2021-03-04 17:31:53 +0100

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.assets.js_compressor = Uglifier.new(harmony: true)
+
+  config.action_mailer.default_url_options = {host: 'https://Gmail-Like.herokuapp.com', port: 3000 }
 end
